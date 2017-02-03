6d39cf7ef6f8df7fcf03b1d7a19ad57d1f5d97fa
Thomas Nelson <tanelso2@gmail.com>
2017-02-03 01:31:52 -0800

    Ok, preprocessing of data done. I think I'm going to stop messing with this and just start populating the redis cache

--- query_server.py
+++ query_server.py
@@ -1,27 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import redis
 
 app = Flask(__name__)
 r = redis.StrictRedis()
 
 def get_bucket_size(bucket_name):
-    total_size = 0
-    for key in r.scan_iter(match="{}/*".format(bucket_name)):
-         total_size += int(r.get(key))
-    return total_size
-    
+    return int(r.zscore("buckets", bucket_name))
+
+def get_all_buckets():
+    return r.zrevrange("buckets", 0, -1, withscores=True)
+
+def name_score_pair_to_dict(pair):
+    return {'name': pair[0].decode('UTF-8'), 'size': int(pair[1])}
 
 @app.route('/buckets')
 def get_buckets():
-    pass
+    return jsonify([name_score_pair_to_dict(p) for p in get_all_buckets()])
 
 @app.route('/buckets/<name>')
 def get_bucket(name):
     return jsonify({'name': name, 'size': get_bucket_size(name)})
 
+def get_folders_at_level(bucket_name, level): 
+    key = "{}:levels:{}".format(bucket_name, level)
+    return r.zrevrange(key, 0, -1, withscores=True)
+
 @app.route('/buckets/<name>/folders')
 def get_folders(name):
-    pass
+    level = request.args.get('level', 1)
+    return jsonify([name_score_pair_to_dict(p) for p in get_folders_at_level(name, level)])
 
 if __name__ == "__main__":
     app.run()
--- s3_poller.py
+++ s3_poller.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 import boto3
+from collections import Counter, defaultdict
 import redis
+
+SIZE = "__size__"
+
+def get_fancy_dict():
+    ret = defaultdict(get_fancy_dict)
+    ret[SIZE] = 0
+    return ret
+
+def add_folder_sizes(name, tree, bucket_name, p, level):
+    set_key = "{bucket}:levels:{level}".format(bucket=bucket_name, level=level)
+    p.zadd(set_key, tree[SIZE], name)
+    for node_name, tree_node in tree.items():
+        if node_name != SIZE:
+            full_name = "{}/{}".format(name, node_name)
+            add_folder_sizes(full_name, tree_node, bucket_name, p, level+1)
 
 r = redis.StrictRedis()
 
@@ -11,6 +27,16 @@ for profile in profiles:
     s3 = session.resource('s3')
     bucket_name = "rsyslog-archives-stg" #TODO: change this to retrieve all bucket names
     bucket = s3.Bucket(bucket_name)
+    bucket_tree = get_fancy_dict()
     for obj in bucket.objects.all():
-        key = bucket_name + "/" + obj.key
-        r.set(key, obj.size)
+        curr_node = bucket_tree
+        folders = obj.key.split('/')[:-1]
+        bucket_tree[SIZE] += obj.size
+        for folder in folders:
+            curr_node = curr_node[folder]
+            curr_node[SIZE] += obj.size
+    r.zadd("buckets", bucket_tree[SIZE], bucket_name)
+    p = r.pipeline()
+    add_folder_sizes(bucket_name, bucket_tree, bucket_name, p, 0)
+    p.execute()
+
