0c91d8adb2f4cb360a484dba1818a19d718d0f52
Thomas Nelson <tanelso2@gmail.com>
2017-02-03 15:30:24 -0800

    Added multiprocessing to s3_poller

--- s3_poller.py
+++ s3_poller.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import boto3
 from collections import Counter, defaultdict
+from multiprocessing import Pool
 import redis
 
 SIZE = "__size__"
@@ -18,8 +19,11 @@ def add_folder_sizes(name, tree, bucket_name, p, level):
             full_name = "{}/{}".format(name, node_name)
             add_folder_sizes(full_name, tree_node, bucket_name, p, level+1)
 
-def store_bucket_data(bucket):
-    bucket_name = bucket.name
+def store_bucket_data(bucket_name):
+    p = redis.StrictRedis().pipeline()
+    session = boto3.Session(profile_name="staging")
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(bucket_name)
     print(bucket_name, end='', flush=True)
     bucket_tree = get_fancy_dict()
     for obj in bucket.objects.all():
@@ -29,8 +33,7 @@ def store_bucket_data(bucket):
         for folder in folders:
             curr_node = curr_node[folder]
             curr_node[SIZE] += obj.size
-    r.zadd("buckets", bucket_tree[SIZE], bucket_name)
-    p = r.pipeline()
+    p.zadd("buckets", bucket_tree[SIZE], bucket_name)
     add_folder_sizes(bucket_name, bucket_tree, bucket_name, p, 0)
     p.execute()
     print("   DONE")
@@ -39,12 +42,18 @@ r = redis.StrictRedis()
 
 # TODO: Add dev and production when we are ready for them
 def main():
+    pool = Pool()
     profiles = ["staging"]
     for profile in profiles:
         session = boto3.Session(profile_name=profile)
         s3 = session.resource('s3')
+        # becuase iterables
+        bucket_names = map(lambda x: x.name, s3.buckets.all())
+        pool.map(store_bucket_data, bucket_names)
+'''
         for bucket in s3.buckets.all():
             store_bucket_data(bucket)
+'''
 
 def get_specific_bucket(bucket_name, profile):
     session = boto3.Session(profile_name=profile)
